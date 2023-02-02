41abf52cd3635efd6375af7b58609ab8550f87d0
gstv1478 <gstv1478@gmail.com>
2023-02-01 21:36:05 -0300

    segundo commit

--- app.js
+++ app.js
@@ -12,25 +12,42 @@ form.addEventListener("submit", function() {
     event.preventDefault();
     const resul = calculadora(aporteInicial.value, aporteMensal.value, media.value, anos.value);
     console.log(resul);
-    const r = "R$" + resul.toFixed(2).replace(".", ",");
+    const r = "R$" + resul.toLocaleString('pt-BR');
+    console.log(r);
     span.textContent = `${r}`;
 });
 
 function calculadora(a, b, c, d) {
-        a = parseInt(a);
-        b = parseInt(b);
-        c = parseInt(c);
-        d = parseInt(d);
-
-        
+        a = parseFloat(a);
+        b = parseFloat(b);
+        c = parseFloat(c);
+        d = parseFloat(d);
 
         // mensal para anual 
         b = b*12;
+
         //rentabilidade
         c = ((1) + (c/100));
-        // rentabilidade do capital inicial
-        a = (a)*(c^(d))  
 
-        let valor = a + b + c + d;
+        a = (a)*(c**(d))  
+
+        if (d == 1) {
+        
+        // rentabilidade do capital inicial
+        a = (a+b)*(c**(d))  
+        console.log(a);
+        } else {
+            /* a = (a)*(c**(d))  */
+
+            for (var i = 0; i < d; i++) {
+                console.log(`${i} vez`);
+                a = a + (b*c**(i+1))
+                console.log(a);
+                
+            }
+            console.log("acabou");
+        }
+        
+        let valor = a
         return valor;
 }
